af3f30751342b635bd77cdb4ef4c13cee7315068
kcroake <kcroake88@gmail.com>
2015-12-12 14:24:22 -0500

    Haven't Pushed In a while
    Adjustments

--- locke/front-page.php
+++ locke/front-page.php
@@ -147,15 +147,73 @@
 	         
         </div>
         
+        
+        
+        
+        
+        
+        
+        
         <div class="container form-wrapper">
-	       <form id="form1" name="form1" class="wufoo topLabel page" accept-charset="UTF-8" autocomplete="off" enctype="multipart/form-data" method="post" novalidate action="https://lockefallsfarm.wufoo.com/forms/zjnffcz18vzq3p/#public">
+         <form id="form1" class="wufoo topLabel page" accept-charset="UTF-8" autocomplete="off" enctype="multipart/form-data" method="post" novalidate="" action="https://lockefallsfarm.wufoo.com/forms/zjnffcz18vzq3p/#public">
+      
+
+        <ul>
+            <li id="foli1" class="notranslate      ">
+            
+            <label class="desc" id="title1" for="Field1">Name <span id="req_1" class="req">*</span></label> 
+            
+            
+            <span><input id="Field1" name="Field1" type="text" class="field text fn" value="" size="8" tabindex="1" required="" placeholder="First Name" > <label for="Field1">First</label></span> <span><input id="Field2" name="Field2" type="text" class="field text ln" value="" size="14" tabindex="2" required="" placeholder="Last Name"> <label for="Field2">Last</label></span></li>
+
+            <li id="foli3" class="notranslate      ">
+                <label class="desc" id="title3" for="Field3">Email <span id="req_3" class="req">*</span></label>
+
+                <div>
+                    <input id="Field3" name="Field3" type="email" placeholder="Email*" spellcheck="false" class="field text medium" value="" maxlength="255" tabindex="3" required="">
+                </div>
+            </li>
+
+            <li id="foli8" class="date notranslate      ">
+            	<label class="desc" id="title8" for="Field8" >Event Date</label>
+            	<span class="month">
+            	<input id="Field8-1" placeholder="MM" name="Field8-1" type="text" class="field text" value="" size="2" maxlength="2" tabindex="4"> 
+            	<label for="Field8-1">MM</label></span> 
+            	<span><input id="Field8-2" placeholder="DD" name="Field8-2" type="text" class="field text" value="" size="2" maxlength="2" tabindex="5">
+            	<label for="Field8-2">DD</label></span>
+            	<span><input id="Field8" placeholder="YYYY" name="Field8" type="text" class="field text" value="" size="4" maxlength="4" tabindex="6"> 
+            	<label for="Field8">YYYY</label></span> <img id="pick8" class="datepicker" src="<?php echo get_stylesheet_directory_uri(); ?>/img/wufoo/calendar.png" alt="Pick a date." data-date-format="yyyy-mm-dd" data-date="2015-12-06"></li>
+
+            <li id="foli5" class="notranslate      ">
+                <label class="desc" id="title5" for="Field5">Message <span id="req_5" class="req">*</span></label>
+
+                <div>
+                    <textarea placeholder="Message" id="Field5" name="Field5" class="field textarea medium" spellcheck="true" rows="10" cols="50" tabindex="7" onkeyup="" required="">
+</textarea>
+                </div>
+            </li>
+
+            <li class="buttons ">
+                <div>
+                    <input id="saveForm" name="saveForm" class="btTxt submit" type="submit" value="Submit">
+                </div>
+            </li>
+
+            <li class="hide"><label for="comment">Do Not Fill This Out</label> 
+            <textarea name="comment" id="comment" rows="1" cols="1">
+</textarea> <input type="hidden" id="idstamp" name="idstamp" value="EC/0VyoY7cg36wD7Mh815aAmDH8FCDGUjlbmp0qGmzw="></li>
+        </ul>
+    </form>
+
+        
+	<!--	<form id="form1" class="wufoo topLabel page" accept-charset="UTF-8" autocomplete="off" enctype="multipart/form-data" method="post" novalidate="" action="https://lockefallsfarm.wufoo.com/forms/zjnffcz18vzq3p/#public">
 				
 				<ul>
 					<li id="foli1" class="notranslate">
 						<label class="desc" id="title1" for="Field1">
 							Name
 						</label>
-							<span>
+							<div>
 								<input id="Field1" name="Field1" type="text" class="field text fn" value="" size="8" tabindex="1" placeholder="First Name" required/>
 								<label for="Field1">First</label>
 							</span>
@@ -204,7 +262,7 @@
 					<input type="hidden" id="idstamp" name="idstamp" value="EC/0VyoY7cg36wD7Mh815aAmDH8FCDGUjlbmp0qGmzw=" />
 					</li>
 					</ul>
-					</form>
+					</form> -->
 	        <div class="contact-info">
 		        <h4>Contact Info</h4>
 		        <p>2531 Chinook Trail<br/>
